ad0b2b3f712135496f411e77e50f8bc49f7d7989
lenny-zanotelli <torigon@DESKTOP-UO71CCD.>
2025-01-13 14:26:22 +0100

    init

---
+++ src/test/java/com/myBlog/myblog/MyBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myBlog.myblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyBlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
