55b6291174b3d71cce1e3cd119492f5237077c1f
lenny-zanotelli <lenny.zanotelli24@gmail.com>
2025-02-12 17:25:41 +0100

    Spring 11 - Contraintes de validation

--- src/main/java/com/myBlog/myblog/exception/GlobalExceptionHandler.java
+++ src/main/java/com/myBlog/myblog/exception/GlobalExceptionHandler.java
@@ -1,7 +1,12 @@
 package com.myBlog.myblog.exception;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -18,6 +23,25 @@ public class GlobalExceptionHandler {
   public ResponseEntity<String> handleGlobalException(Exception exception) {
     return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Une erreur est survenue.");
   }
+
+  
+  @ExceptionHandler(MethodArgumentNotValidException.class)
+  public ResponseEntity<Map<String, String>> handleValidationExceptions(MethodArgumentNotValidException ex) {
+
+    Map<String, String> errors = new HashMap<>();
+
+    ex.getBindingResult().getAllErrors().forEach((error) -> {
+
+        String fieldName = ((FieldError) error).getField();
+
+        String errorMessage = error.getDefaultMessage();
+
+        errors.put(fieldName, errorMessage);
+
+    });
+
+    return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
+}
 
   
 }
--- src/main/java/com/myBlog/myblog/mapper/ArticleMapper.java
+++ src/main/java/com/myBlog/myblog/mapper/ArticleMapper.java
@@ -1,16 +1,59 @@
 package com.myBlog.myblog.mapper;
 
+import java.util.List;
 import java.util.stream.Collectors;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import com.myBlog.myblog.DTO.ArticleCreateDTO;
 import com.myBlog.myblog.DTO.ArticleDTO;
 import com.myBlog.myblog.DTO.AuthorDTO;
+import com.myBlog.myblog.exception.ResourceNotFoundException;
 import com.myBlog.myblog.model.Article;
+import com.myBlog.myblog.model.ArticleAuthor;
+import com.myBlog.myblog.model.Author;
+import com.myBlog.myblog.model.Category;
 import com.myBlog.myblog.model.Image;
+import com.myBlog.myblog.repository.AuthorRepository;
+import com.myBlog.myblog.repository.CategoryRepository;
+import com.myBlog.myblog.repository.ImageRepository;
 
 @Component
 public class ArticleMapper {
+    public Article convertToEntity(ArticleCreateDTO dto) {
+      Article article = new Article();
+      article.setTitle(dto.getTitle());
+      article.setContent(dto.getContent());
+
+      Category category = new Category();
+      category.setId(dto.getCategoryId());
+      article.setCategory(category);
+
+      List<Image> images = dto.getImages().stream()
+        .map(imageDTO -> {
+        Image image = new Image();
+        image.setUrl(imageDTO.getUrl());
+        return image;
+        })
+        .collect(Collectors.toList());
+        article.setImages(images);
+
+      List<ArticleAuthor> articleAuthors = dto.getAuthors().stream()
+        .map(authorDTO -> {
+          ArticleAuthor articleAuthor = new ArticleAuthor();
+
+          Author author = new Author();
+          author.setId(authorDTO.getAuthorId());
+          articleAuthor.setAuthor(author);
+          articleAuthor.setContribution(authorDTO.getContribution());
+          articleAuthor.setArticle(article);
+          return articleAuthor;
+        }).collect(Collectors.toList());
+        article.setArticleAuthors(articleAuthors);
+
+      return article;
+    }
 
     public ArticleDTO convertToDTO(Article article) {
       ArticleDTO articleDTO = new ArticleDTO();
