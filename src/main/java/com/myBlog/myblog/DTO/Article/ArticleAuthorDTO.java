3fb43e3a44f108acbf15d6e6e260a665c10e9155
lenny-zanotelli <mysubscriptions@tuta.com>
2025-04-09 16:03:14 +0200

    refactor(DTO): create folder for each DTOs entities

---
+++ src/main/java/com/myBlog/myblog/DTO/Article/ArticleAuthorDTO.java
@@ -0,0 +1,48 @@
+package com.myBlog.myblog.DTO.Article;
+
+import com.myBlog.myblog.model.Article;
+import com.myBlog.myblog.model.Author;
+
+public class ArticleAuthorDTO {
+  private Long id;
+  private Article article;
+  private Author author;
+  private String contribution;
+
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+
+    this.id = id;
+  }
+
+  public Article getArticle() {
+    return article;
+  }
+
+  public void setArticle(Article article) {
+    this.article = article;
+  }
+
+  public Author getAuthor() {
+    return author;
+  }
+
+  public void setAuthor(Author author) {
+    this.author = author;
+  }
+
+  public String getContribution() {
+
+    return contribution;
+  }
+
+  public void setContribution(String contribution) {
+
+    this.contribution = contribution;
+  }
+  
+}
---
+++ src/main/java/com/myBlog/myblog/DTO/Author/AuthorDTO.java
@@ -0,0 +1,45 @@
+package com.myBlog.myblog.DTO.Author;
+
+import java.util.List;
+
+public class AuthorDTO {
+  private Long id;
+  private String firstname;
+  private String lastname;
+  private List<Long> articleIds;
+
+
+  public Long getId() {
+    return id;
+  }
+  
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public String getFirstname() {
+    return firstname;
+  }
+
+  public void setFirstname(String firstname) {
+    this.firstname = firstname;
+  }
+
+  public String getLastname() {
+    return lastname;
+  }
+
+
+  public void setLastname(String lastname) {
+    this.lastname = lastname;
+  }
+
+  public List<Long> getArticleIds() {
+    return articleIds;
+  }
+
+  public void setArticleIds(List<Long> articleIds) {
+    this.articleIds = articleIds;
+  }
+  
+}
